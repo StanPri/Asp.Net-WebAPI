08395260bc257087b3809d85f572fb12931b9dd6
StanPri <spribytkovsky@yahoo.com>
2017-02-01 16:51:01 -0800

    ProductController in WebAPI added

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET api/product
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/product/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/product
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/product/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/product/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
