1458cd51517bf0027d63175a4175dd68f949a333
StanPri <spribytkovsky@yahoo.com>
2017-02-01 16:16:14 -0800

    Cleaning

---
+++ BusinessServices/ProductServices.cs
@@ -0,0 +1,23 @@
+﻿using DataModel.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessServices
+{
+    class ProductServices : IProductServices
+    {
+
+        private readonly UnitOfWork _unitOfWork;
+
+        /// <summary>
+        /// Public constructor.
+        /// </summary>
+        public ProductServices()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+    }
+}
